98c1e4bf3e1bfad84f7eb1e2ac0b9ffb91e3ffe9
mateus <mateusmacedoeng@gmail.com>
2025-09-03 12:48:49 -0300

    add dialog component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import InputCheckbox from "./components/input-checkbox";
 import InputSingleFile from "./components/input-single-file";
 import { useForm } from "react-hook-form";
 import ImageFilePreview from "./components/image-file-preview";
+import { Dialog, DialogBody, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from "./components/dialog";
+import Text from "./components/text";
 
 export default function App() {
 	const form = useForm();
@@ -72,7 +74,36 @@ export default function App() {
 				form={form} 
 				allowedExtensions={['png', 'jpg', 'jpeg', 'webp']} maxFileSizeInMB={50} 
 				replaceBy={<ImageFilePreview src={fileSource} alt="Image"/>}
-				{...form.register("file")}/>
+				{...form.register("file")}
+				/>
+			</div>
+
+			<div>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button>Abrir Modal</Button>
+					</DialogTrigger>
+					<DialogContent>
+						<DialogHeader>
+							teste dialog
+						</DialogHeader>
+						<DialogBody>
+							<Text className="mb-4" as="div">teste conteúdo do dialog</Text>
+							<InputSingleFile 
+							form={form} 
+							allowedExtensions={['png', 'jpg', 'jpeg', 'webp']} maxFileSizeInMB={50} 
+							replaceBy={<ImageFilePreview src={fileSource} alt="Image"/>}
+							{...form.register("file")}
+							/>
+						</DialogBody>
+						<DialogFooter>
+							<DialogClose asChild>
+								<Button variant="secondary">Cancelar</Button>
+							</DialogClose>
+							<Button>Adicionar</Button>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
 			</div>
 		</div>
 	);
