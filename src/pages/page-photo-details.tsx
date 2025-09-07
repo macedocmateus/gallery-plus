8c299731408c94b9aa4a7c61dcdeb85c242517f3
mateus <mateusmacedoeng@gmail.com>
2025-09-07 20:49:14 -0300

    header component on the photo details page

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,18 +1,37 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 export default function PagePhotoDetails() {
     const {id} = useParams()
     
+    const isLoadingPhoto = false
+    const photo = {
+         id: '123',
+         title: 'Olá mundo',
+         imageId: "portrait-tower.png",
+         albums: [
+                   {id: '123', title: 'Album 1'},
+                   {id: '321', title: 'Album 2'},
+                   {id: '213', title: 'Album 3'},
+                   {id: '312', title: 'Album 4'}
+                ]
+    } as Photo
+    
     return (
-            <>
-                <Text variant="heading-large">
-                    Página detalhe da foto
-                </Text>
-                <hr />
-                <Text variant="heading-large">
-                    ID da foto:{id}
-                </Text>
-            </>
+            <Container>
+                <header className="flex items-center justify-between gap-8 mb-8">
+                {!isLoadingPhoto ? (
+                    <Text variant="heading-large">{photo?.title}</Text>
+                ) : (
+                    <Skeleton className="w-48 h-8"/>
+                )}
+
+                <PhotosNavigator loading={isLoadingPhoto}/>
+                </header>
+            </Container>
         )
 }
