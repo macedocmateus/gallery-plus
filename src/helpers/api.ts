ed61387ba3615a097021d5fd8fcc9d95651a4444
mateus <mateusmacedoeng@gmail.com>
2025-09-09 16:20:08 -0300

    add and config axios

---
+++ src/helpers/api.ts
@@ -0,0 +1,11 @@
+import axios, {type AxiosRequestConfig } from 'axios'
+
+// Chamar as APIs, post, put, patch e delete
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+// Chamar exclusivamente o get
+export function fetcher(url:string, options: AxiosRequestConfig = {}) {
+    api.get(url, options).then((res) => res.data)
+}
