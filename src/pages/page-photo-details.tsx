10bc903446ce34519cceae892a4880d17d71b1e9
mateus <mateusmacedoeng@gmail.com>
2025-09-11 12:53:26 -0300

    add previous and next photo

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -12,7 +12,7 @@ import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PagePhotoDetails() {
     const {id} = useParams()
-    const {photo, isLoadingPhoto} = usePhoto(id)
+    const {photo, isLoadingPhoto, previousPhotoId, nextPhotoId} = usePhoto(id)
     const {albums, isLoadingAlbums} = useAlbums()
 
     if (!isLoadingPhoto && !photo) {
@@ -29,7 +29,12 @@ export default function PagePhotoDetails() {
                     <Skeleton className="w-48 h-8"/>
                 )}
 
-                <PhotosNavigator loading={isLoadingPhoto}/>
+                <PhotosNavigator 
+                previousPhotoId={previousPhotoId} 
+                nextPhotoId={nextPhotoId}
+                loading={isLoadingPhoto}
+                
+                />
                 </header>
 
                 <div className="grid grid-cols-[21rem_1fr] gap-24">
